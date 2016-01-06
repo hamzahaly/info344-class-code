5f4a05070d93e65bfc535ccd502a7533dd5d38a4
hamzahaly <hzhaly93@gmail.com>
2016-01-06 10:20:41 -0800

    PHP notes

---
+++ php/test.php
@@ -0,0 +1,26 @@
+Hey this is some content above the code
+<?php
+$name = 'Hamz';
+$fullName = $name . '117';
+
+
+class Person {
+	protected $name;
+	
+	public function __construct($n) {
+		$this->name = $n; 
+	}	
+	
+	public function getName() {
+		return $this->name;
+	}
+}
+
+function foo($bar) {
+	echo "Hey this is the foo fighting function\n";
+}
+
+echo "Hello {$name}!\n";
+foo(NULL);
+?>
+And this is some content below
---
+++ php/warmup.php
@@ -0,0 +1,20 @@
+<?php
+$randomInt = rand(1, 100);
+
+echo "your new random value is $randomInt\n";
+
+date_default_timezone_set("AMERICA/Los_Angeles");
+$months = array();
+
+for ($i = 1; $i <= 12; $i++) {
+	$months[$i] = date(F, mktime(0, 0, 0, $i));
+	echo "$months[$i] ";
+}
+sort($months);
+
+for ($i = 1; $i <= 12; $i++) {
+	$months[$i] = date(F, mktime(0, 0, 0, $i));
+	echo "$months[$i] ";
+}
+
+?>
